3f7f7b3bd3e5a36ba7f658ae9968a1a3c7141a93
roniemanaongsong-afk <ronie.manaongsong@dorsu.edu.ph>
2025-09-04 13:18:13 +0800

    Update Exer1_Declarative.java

--- EXER1_Manaongsong/Exer1_Declarative.java
+++ EXER1_Manaongsong/Exer1_Declarative.java
@@ -1,1 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
 
+public class Exer1_Declarative {
+    public static void main(String[] args) {
+        // Calculate sum of even numbers in declarative style
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        
+        int sum = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        
+        System.out.println("Sum of even numbers (Declarative): " + sum);
+    }
+}
