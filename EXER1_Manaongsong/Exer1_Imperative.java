09f7c454a6dfad27e362cbea7e2ab44a4cf271f9
roniemanaongsong-afk <ronie.manaongsong@dorsu.edu.ph>
2025-09-04 13:17:22 +0800

    Update Exer1_Imperative.java

--- EXER1_Manaongsong/Exer1_Imperative.java
+++ EXER1_Manaongsong/Exer1_Imperative.java
@@ -1,1 +1,15 @@
-
+public class Exer1_Imperative {
+    public static void main(String[] args) {
+        // Calculate sum of even numbers in imperative style
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
+        }
+        
+        System.out.println("Sum of even numbers (Imperative): " + sum);
+    }
+}
