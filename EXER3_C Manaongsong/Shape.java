deb15f3d5a3ef0018beb6ccd59385b8edc361c8d
roniemanaongsong-afk <ronie.manaongsong@dorsu.edu.ph>
2026-01-04 20:11:34 +0800

    Create Shape.java

---
+++ EXER3_C Manaongsong/Shape.java
@@ -0,0 +1,26 @@
+public class Shape {
+
+        float area;
+
+        public Shape() {
+        this.area = 0.0f;
+    }
+
+
+    public float calculateArea() {
+        return 0.0f;
+
+    };
+
+    public String displayInfo() {
+        return null;
+    }
+    public float getArea(){
+        return this.area;
+
+    }
+    public void setArea(float area){
+        this.area = area;
+
+    }
+}
